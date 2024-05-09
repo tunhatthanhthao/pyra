ea1d8ce2622cf8d77441252d67e8763f5f2a9d75
ThaoTu <69448536+tunhatthanhthao@users.noreply.github.com>
2024-05-09 12:12:05 -0500

    Update extend_B_mongo.py

--- extend_B_mongo.py
+++ extend_B_mongo.py
@@ -21,17 +21,6 @@ class Timer(object):
         if self.verbose:
             print('elapsed time: %f ms' % self.msecs)
 
-def find_union(A, B):
-    union_intervals = []
-    for interval_a in A:
-        if interval_a[0] is None or interval_a[1] is None:
-            continue
-        for interval_b in B:
-            if interval_b[0] is None or interval_b[1] is None:
-                continue
-            if interval_a[0] < interval_b[0] and interval_a[1] > interval_b[1]:
-                union_intervals.append((interval_a, interval_b))
-    return union_intervals
 
 
 def print_intervals_without_gaps(intervals):
@@ -96,23 +85,6 @@ def read_intervals_from_mongodb(collection, table_name):
     except Exception as e:
         print(f"Error reading from MongoDB: {e}")
         return []
-
-def union(A, B):
-    result_set = set()
-    for extent_a in A:
-        for extent_b in B:
-            if not extent_a or not extent_b:
-                continue  # Skip invalid intervals
-            # Check for None values and handle them appropriately
-            if extent_b[1] is None or extent_a[0] is None:
-                continue  # Skip if any endpoint is None
-            elif extent_b[1] < extent_a[0]:
-                continue  # No overlap, skip to the next pair
-            else:
-                # There is an overlap, add both intervals to the result set
-                result_set.add(tuple(extent_a))
-                result_set.add(tuple(extent_b))
-    return sorted(result_set, key=lambda x: (x[0], x[1]))
 
 
 def select_tables_to_compare(tables):
